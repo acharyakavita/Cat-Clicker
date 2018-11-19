c273bf3fdbf9bf9c8488b6e61435ed60e5466ebc
acharyakavita <acharyakavita12@gmail.com>
2018-11-19 13:22:30 -0500

    2nd requirement to add 2 cat images. I have used ES6 and object oriented concepts to add any number of cat objects.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
- (function imgClick(){
-     let clickNumber=0;
-    const catImg=document.querySelector('.cat');
-    catImg.addEventListener('click',function(){
-         console.log(++clickNumber);
-    })
- })();
+class Cat {
+    constructor(name, url, imgClassName, clickCountClassName) {
+        this.name = name;
+        this.clickCount = 0;
+        this.url = url;
+        this.imgClassName = imgClassName;
+        this.clickCountClassName = clickCountClassName;
+    }
+    updateName() {
+        let main = document.querySelector('main');
+        let div = document.createElement('div');
+        div.innerHTML += `<div><h3>${this.name}</h3>
+                        <img class="cat ${this.imgClassName}" src="${this.url}" alt="cute cat">
+                        <p>Click Count=<span class="${this.clickCountClassName}">${this.clickCount}</span></p</div>`;
+        main.appendChild(div);
+        
+    }
+    updateClick(imgclassName, clickCountClassName) {
+        document.querySelector(imgclassName).addEventListener('click', function () {
+            this.clickCount++;
+            document.querySelector(clickCountClassName).textContent = this.clickCount;
+        }.bind(this))
+
+    }
+
+}
+
+const cat1 = new Cat('Tommy', 'images/cat.jpg', 'image1', 'click-Count1');
+const cat2 = new Cat('shaila', 'images/cat2.jpg', 'image2', 'click-Count2');
+cat1.updateName();
+cat2.updateName();
+cat1.updateClick('.image1', '.click-Count1');
+cat2.updateClick('.image2', '.click-Count2');
