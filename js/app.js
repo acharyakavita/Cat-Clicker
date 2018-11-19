7a4a87dc6740197114dd5102d6d5e49652c71690
acharyakavita <acharyakavita12@gmail.com>
2018-11-19 18:25:27 -0500

    Requirement 3 changes. Visuals The application should display
    1. a list of at least 5 cats, listed by name
    2. an area to display the selected cat
    3. In the cat display area, the following should be displayed
    
    a. the cat's name
    b. a picture of the cat
    c. text showing the number of clicks
    d. The specifics of the layout do not matter, so style it however you'd like.
    Interaction
    When a cat name is clicked in the list, the cat display area should update to show the data for the selected cat.
    The number of clicks in the cat area should be unique to each cat, and should increment when the cat's picture is clicked.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,76 @@
 class Cat {
-    constructor(name, url, imgClassName, clickCountClassName) {
+    constructor(name, url) {
         this.name = name;
         this.clickCount = 0;
         this.url = url;
-        this.imgClassName = imgClassName;
-        this.clickCountClassName = clickCountClassName;
+        this.select = null;
+        this.display = false;
     }
+
+
     updateName() {
-        let main = document.querySelector('main');
-        let div = document.createElement('div');
-        div.innerHTML += `<div><h3>${this.name}</h3>
-                        <img class="cat ${this.imgClassName}" src="${this.url}" alt="cute cat">
-                        <p>Click Count=<span class="${this.clickCountClassName}">${this.clickCount}</span></p</div>`;
-        main.appendChild(div);
-        
-    }
-    updateClick(imgclassName, clickCountClassName) {
-        document.querySelector(imgclassName).addEventListener('click', function () {
-            this.clickCount++;
-            document.querySelector(clickCountClassName).textContent = this.clickCount;
+        this.select = document.querySelector('select');
+        this.select.addEventListener('change', function (event) {
+            let main = document.querySelector('main');
+            let image = document.querySelector('.image');
+            this.display = false;
+
+            switch (event.target.value) {
+                case 'default':
+                    if (image !== null) {
+                        let item = main.lastElementChild;
+                        main.removeChild(item);
+                        image = document.querySelector('.image');
+                    }
+                    break;
+                case 'Tommy':
+                case 'Shaila':
+                case 'Jerry':
+                case 'Truffles':
+                case 'Laila':
+                    if (image !== null && this.name === event.target.value) {
+                        let item = main.lastElementChild;
+                        main.removeChild(item);
+                        image = document.querySelector('.image');
+                    }
+                    if (image === null && this.name === event.target.value) {
+                        this.count = 0;
+                        let div = document.createElement('div');
+                        div.classList.add('image')
+                        div.innerHTML += `<h3>${this.name}</h3>
+                             <img class="cat" src="${this.url}" alt="cute cat">
+                             <p>Click Count=<span class="clickCount">${this.clickCount}</span></p>`;
+                        main.appendChild(div);
+                        this.display = true;
+                        this.updateClick()
+                    }
+                    break;
+
+            }
         }.bind(this))
+
+
+    }
+    updateClick() {
+        if (this.display === true) {
+            document.querySelector('.cat').addEventListener('click', function () {
+                this.clickCount++;
+                document.querySelector('.clickCount').textContent = this.clickCount;
+            }.bind(this))
+
+        }
 
     }
 
 }
 
-const cat1 = new Cat('Tommy', 'images/cat.jpg', 'image1', 'click-Count1');
-const cat2 = new Cat('shaila', 'images/cat2.jpg', 'image2', 'click-Count2');
+const cat1 = new Cat('Tommy', 'images/cat1.jpg');
+const cat2 = new Cat('Shaila', 'images/cat2.jpg');
+const cat3 = new Cat('Jerry', 'images/cat3.jpg');
+const cat4 = new Cat('Truffles', 'images/cat4.jpg');
+const cat5 = new Cat('Laila', 'images/cat5.jpg');
 cat1.updateName();
 cat2.updateName();
-cat1.updateClick('.image1', '.click-Count1');
-cat2.updateClick('.image2', '.click-Count2');
+cat3.updateName();
+cat4.updateName();
+cat5.updateName();
